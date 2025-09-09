8c208948863cad28320558aebdf1d8e995f90302
Robstradamus <robstradamous@hotmail.com>
2025-09-09 17:42:52 -0300

    Ejercicio 2: Colores de Resistencia

---
+++ ejercicios/ejercicio_2/js/app.js
@@ -0,0 +1,34 @@
+function color(op){
+    switch(op){
+        case 0:
+            alert('#000000');
+            break;
+        case 1:
+            alert('#964800');
+            break;
+        case 2:
+            alert('#FF0000');
+            break;
+        case 3:
+            alert('#FFA500');
+            break;
+        case 4:
+            alert('#FFFF00');
+            break;
+        case 5:
+            alert('#008000');
+            break;
+        case 6:
+            alert('#0000FF');
+            break;
+        case 7:
+            alert('#800080');
+            break;
+        case 8:
+            alert('#808080');
+            break;
+        case 9:
+            alert('FFFFFF;');
+            break;
+    }
+}
