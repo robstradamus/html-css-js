66982e65b295c2b3ffe52166f5bbdbdf8a672d0c
Robstradamus <robstradamous@hotmail.com>
2025-09-09 17:43:30 -0300

    Ejercicio 3: Colores de Semaforo

---
+++ ejercicios/ejercicio_3/js/app.js
@@ -0,0 +1,13 @@
+function color(op){
+    switch(op){
+        case 0:
+            alert('ROJO');
+            break;
+        case 1:
+            alert('AMARILLO');
+            break;
+        case 2:
+            alert('VERDE');
+            break;
+    }
+}
